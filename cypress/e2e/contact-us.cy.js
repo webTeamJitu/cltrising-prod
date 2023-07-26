201f01407e15b043926b52d8b9c4a02ddc6ffd0f
Fidelis Wairimu <fidelis.wairimu@acquiretek.com>
2023-07-26 11:51:43 +0300

    contact-us test

--- cypress/e2e/contact-us.cy.js
+++ cypress/e2e/contact-us.cy.js
@@ -16,39 +16,34 @@ describe("Contact us form test case", () => {
 
     it("Input field interaction", () => {
 
+        
+        cy.readJson('cypress/fixtures/contact-us.json').then((jsonData) => {
 
-        cy.waitUntilDataLoaded().then(() => {
-            cy.readJson('cypress/fixtures/contact-us.json').then((jsonData) => {
-                expect(jsonData).not.be.empty;
-                // expect(jsonData).to.have.property('key', 'expectedValue');
-                const data = jsonData.form_test
-    
-                cy.get('#wpforms-364-field_0')
-                    .type(`${data.firstName}`)
-                    .should("be.visible", "have.value", `${data.firstName}`)
-    
-                    .get('#wpforms-364-field_0-last')
-                    .type(`${data.lastName}`)
-                    .should("be.visible", "have.value", `${data.lastName}`)
-    
-                    .get('#wpforms-364-field_1')
-                    .type(`${data.email}`)
-                    .should("be.visible", "have.value", `${data.email}`)
-    
-                    .get('#wpforms-364-field_2')
-                    .type(`${data.message}`)
-                    .should("be.visible", "have.value", `${data.message}`)
-    
-                    .get('.g-recaptcha')
-                    .find("iframe[title='reCAPTCHA']")
-                    .should("be.visible")
-                    
-                    .get('.wpforms-submit-container')
-                    .find("button[name='wpforms[submit]']")
-                    .should("be.visible")
-            });
-          });
+            const data = jsonData.form_test
 
+            cy.get('#wpforms-364-field_0')
+                .type(`${data.firstName}`)
+                .should("be.visible", "have.value", `${data.firstName}`)
 
+                .get('#wpforms-364-field_0-last')
+                .type(`${data.lastName}`)
+                .should("be.visible", "have.value", `${data.lastName}`)
+
+                .get('#wpforms-364-field_1')
+                .type(`${data.email}`)
+                .should("be.visible", "have.value", `${data.email}`)
+
+                .get('#wpforms-364-field_2')
+                .type(`${data.message}`)
+                .should("be.visible", "have.value", `${data.message}`)
+
+                .get('.g-recaptcha')
+                .find("iframe[title='reCAPTCHA']")
+                .should("be.visible")
+                
+                .get('.wpforms-submit-container')
+                .find("button[name='wpforms[submit]']")
+                .should("be.visible")
+        });
     })
 })
